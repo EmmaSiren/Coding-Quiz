07c4e95332ae5c5ff66324f878b4d67de3b9e5da
Emma Velazquez <emmasiren24@gmail.com>
2022-08-07 23:44:33 -0700

    Added HTML files for quiz navigation. Stylized each file in CSS.

---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+var questions = [{
+    id: 0,
+    q: "What is the answer?",
+    a: [{ text: "One", isCorrect: false}, 
+        { text: "Two", isCorrect: false},
+        { text: "Three", isCorrect: false},
+        { text: "Four", isCorrect: true}
+    ]
+},
+{
+    id: 1,
+    q: "What is the answer?",
+    a: [{ text: "One", isCorrect: false}, 
+        { text: "Two", isCorrect: false},
+        { text: "Three", isCorrect: false},
+        { text: "Four", isCorrect: true}
+    ]
+},
+{
+    id: 2,
+    q: "What is the answer?",
+    a: [{ text: "One", isCorrect: false}, 
+        { text: "Two", isCorrect: false},
+        { text: "Three", isCorrect: false},
+        { text: "Four", isCorrect: true}
+    ]
+}
+
+]
+
